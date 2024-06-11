775bcfa658e0902ae204dd113f1c1d423c0891fe
Johannes Eimer <johannes.eimer@jep-dev.com>
2024-06-11 04:24:23 +0200

    feat: add optional Linie-Hafen-Fahrplan-ID and linie_id fix: use correct spelling for the station_id

--- travelmanager/modules/relation.py
+++ travelmanager/modules/relation.py
@@ -3,9 +3,13 @@ from ..travelmanager_api import TravelManagerAPI
 
 class Relation:
     @staticmethod
-    def get(station_id, date):
+    def get(station_id, date, lhf_id=None, linie_id=None):
         params = {
-            "station": station_id,
+            "station_id": station_id,
             "date": date,
         }
+        if lhf_id:
+            params["lhf_id"] = lhf_id
+        if linie_id:
+            params["linie_id"] = linie_id
         return TravelManagerAPI.get("relations", params)
